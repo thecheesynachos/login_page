e1236df9a522a7680e68a626d17e2caf2d69d449
Apivich Hemachandra <apivich.hemachandra@gmail.com>
2019-05-27 09:59:49 +0700

    First commit

---
+++ src/main/java/io/muic/ooc/webapp/Webapp.java
@@ -0,0 +1,38 @@
+package io.muic.ooc.webapp;
+
+import java.io.File;
+import javax.servlet.ServletException;
+
+import io.muic.ooc.webapp.service.SecurityService;
+import io.muic.ooc.webapp.servlet.HomeServlet;
+import io.muic.ooc.webapp.servlet.LoginServlet;
+import org.apache.catalina.Context;
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.startup.Tomcat;
+
+public class Webapp {
+
+    public static void main(String[] args) {
+
+        File docBase = new File("src/main/webapp/");
+        docBase.mkdirs();
+        Tomcat tomcat = new Tomcat();
+        tomcat.setPort(8082);
+
+        SecurityService securityService = new SecurityService();
+        ServletRouter servletRouter = new ServletRouter();
+        servletRouter.setSecurityService(securityService);
+
+        Context ctx;
+        try {
+            ctx = tomcat.addWebapp("", docBase.getAbsolutePath());
+            servletRouter.init(ctx);
+
+            tomcat.start();
+            tomcat.getServer().await();
+        } catch (ServletException | LifecycleException ex) {
+            ex.printStackTrace();
+        }
+
+    }
+}
